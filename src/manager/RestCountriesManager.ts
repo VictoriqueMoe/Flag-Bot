7f2d7918ebc92335b0189eb08bc94cfdfdb69f60
VictoriqueMoe <loli@victorique.moe>
2024-09-14 03:17:23 +0100

    ignore white

--- src/manager/RestCountriesManager.ts
+++ src/manager/RestCountriesManager.ts
@@ -114,6 +114,7 @@ export class RestCountriesManager {
         }
         const colourInfo = await getAverageColor(url, {
             algorithm: "dominant",
+            ignoredColor: [255, 255, 255, 255],
         });
         const hex = colourInfo.hex as HexColorString;
         this.colourCache.set(url, hex);
